d46490d15680692359d84101f7c54a18fa7debd2
Chavez0296 <luis.chavez1996@gmail.com>
2024-09-12 09:14:23 -0700

    third

--- bookInvMngSys.c
+++ bookInvMngSys.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 
 int main(void){
-    int numOfBooks = 0;
-    int update = 0;
+    int numOfBooks = 0; //int variable to hold the number of books
+    int update = 0; //int update will hold user input to add a positive or negative number
     while(1){
-        printf("Total number of books: %d\n", numOfBooks);
-        printf("Enter the number of books to add or remove (negative number) or enter 0 to quit: ");
-        scanf("%d", &update);
-        if(update == 0){
-            break;
+        printf("Total number of books: %d\n", numOfBooks); //print numOfBooks to user
+        printf("Enter the number of books to add or remove (negative number) or enter 0 to quit: "); //displays prompt to user on how the program works. 
+        scanf("%d", &update); //take user input of adding or removing books
+        if(update == 0){ //if update is 0 
+            break; //then exit
         }
         else{
-            numOfBooks += update;
+            numOfBooks += update; //add the positive or negative number to numOfBooks in the inventory
         }
     }
 }
--- calculator.c
+++ calculator.c
@@ -1,34 +1,34 @@
 #include <stdio.h>
-int add(int x, int y, int z);
+int add(int x, int y, int z); // Function declarations for the calculator to add, subtract, multiply and divide
 int subtract(int x, int y, int z);
 int divide(int x, int y);
 int multiply(int x, int y);
 
 int main(void)
 {
-    int expression = divide(multiply(add(1,1,1),1),multiply(subtract(1,4,6),1));
-    printf("The expression calculated is: %d\n", expression);
+    int expression = divide(multiply(add(1,1,1),1),multiply(subtract(1,4,6),1)); //creating an int variable to hold the returned value of calculations that are done with the functions 
+    printf("The expression calculated is: %d\n", expression);    //prints the value of the expression
 
-    int undefined = divide(3,0);
-    printf("%d", undefined);
+    int undefined = divide(3,0); //showing how the divide function works with the dividing by 0 test case
+    printf("%d", undefined);    //prints the value of dividing by 0
 }
 
 int add(int x, int y, int z){
-    return x + y + z;
+    return x + y + z; //returns the additions of 3 int variables passed by value (copy)
 }
 
 int subtract(int x, int y, int z){
-    return z - y - x;
+    return z - y - x; //subtracts from z - y - x order 
 }
 
 int divide(int x, int y){
     if(y == 0){
         printf("%s", "Having a denominator of 0 will reult in a ZeroDivisionError!\n");
-        return -1;
+        return -1; //if the demoninator is 0 then it prints the above result, but must return a number hence returning -1
     }
-    return x / y;
+    return x / y; //returns the division of x/y
 }
 
 int multiply(int x, int y){
-    return x * y;
+    return x * y; //multiplies x * y values 
 }
